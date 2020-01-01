5759fa27bffe54fee619183bf9ceb62a33e98d9c
smthnspcl <pascal@eberlein.io>
2020-01-01 22:46:34 +0100

    update

--- srv.py
+++ srv.py
@@ -1,28 +1,27 @@
-from flask import Flask, Response, request
+from sanic import Sanic, response
 from argparse import ArgumentParser
 import requests
 
-app = Flask(__name__)
-
 ap = ArgumentParser()
-ap.add_argument("-h", "--host", type=str, help="host to run on")
-ap.add_argument("-p", "--port", type=int, help="port to run on")
+ap.add_argument("--host", type=str, help="host to run on", default="0.0.0.0")
+ap.add_argument("--port", type=int, help="port to run on", default=8080)
 ap.add_argument("--only-from", type=str, help="only from domain")
 args = ap.parse_args()
 
+app = Sanic(__name__)
+
 
 @app.route("/")
-def root():
-    return Response("cors proxy")  # todo actual info site and how to use this thing
+async def root(req):
+    return response.text("cors proxy")  # todo actual info site and how to use this thing
 
 
 @app.route("/proxy/<path:path>")
-def proxy(path):  # todo validate it is a url
-    
-    r = Response(requests.get(path))  # todo post/etc requests?
-    r.headers['Access-Control-Allow-Origin'] = "*"
+async def proxy(req, path):  # todo validate it is a url
+    r = response.html(requests.get(path).content.decode("utf-8"))  # todo post/etc requests?
+    r.headers.add('Access-Control-Allow-Origin', "*")
     return r
 
 
 if __name__ == '__main__':
-    app.run(args.host, args.port, threaded=True)
+    app.run(args.host, args.port)
